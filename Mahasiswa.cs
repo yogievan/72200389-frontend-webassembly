8957fa41eb508ef56099dfee8b1ad72272084704
yogievan <yogievan32@gmail.com>
2022-10-13 16:55:06 +0700

    Pertemuan 7

---
+++ Mahasiswa.cs
@@ -0,0 +1,15 @@
+public class Mahasiswa
+    {
+        public int id { get; set; }
+
+        public string? nama { get; set; }
+
+        public string? nim { get; set; }
+
+        public string? alamat { get; set; }
+
+        public string? email { get; set; }
+
+        public string? foto { get; set; }
+        public string? nimProgmob { get; set; }
+    }
